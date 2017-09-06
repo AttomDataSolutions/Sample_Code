b414e74cadc75a2b31f0b9d5ce59990c6815eeb6
Kevin Mattice <kmattice@onboardinformatics.com>
2017-09-06 15:05:06 -0400

    Property Detail Call Sample C# code
    Sample code for making a /property/detail call to the Onboard API in C#

---
+++ Property_Detail_Call.cs
@@ -0,0 +1,5 @@
+var client = new RestClient("https://search.onboard-apis.com/propertyapi/v1.0.0/property/detail?address1=4529%20Winona%20Court&address2=Denver%2C%20CO");
+var request = new RestRequest(Method.GET); 
+request.AddHeader("apikey", "870e26a0ffcc29cbb6b8bc86012a8c28"); 
+request.AddHeader("accept", "application/json"); 
+IRestResponse response = client.Execute(request);
