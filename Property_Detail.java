50d4f3527211815782f652582615b51fe5757b81
Kevin Mattice <kmattice@onboardinformatics.com>
2017-09-13 15:48:33 -0400

    Create Property_Detail.java
    Sample code for making a property detail call using Java

---
+++ Property_Detail.java
@@ -0,0 +1,11 @@
+OkHttpClient client = new OkHttpClient(); 
+
+Request request = new Request.Builder() 
+  .url("https://search.onboard-apis.com/propertyapi/v1.0.0/property/detail?address1=4529%20Winona%20Court&address2=Denver%2C%20CO")
+  .get() 
+  .addHeader("accept", "application/json") 
+  .addHeader("apikey", "870e26a0ffcc29cbb6b8bc86012a8c28") 
+
+  .build(); 
+
+Response response = client.newCall(request).execute();
