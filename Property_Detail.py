8c7d8ab50538ba351c2ca9efa13570ebd7f12cd9
Kevin Mattice <kmattice@onboardinformatics.com>
2017-09-13 15:50:23 -0400

    Create Property_Detail.py
    Sample code for making a property detail call in Python

---
+++ Property_Detail.py
@@ -0,0 +1,15 @@
+import http.client 
+
+conn = http.client.HTTPSConnection("search.onboard-apis.com") 
+
+headers = { 
+    'accept': "application/json", 
+    'apikey': "870e26a0ffcc29cbb6b8bc86012a8c28", 
+    } 
+
+conn.request("GET", "/propertyapi/v1.0.0/property/detail?address1=4529%20Winona%20Court&address2=Denver%2C%20CO", headers=headers) 
+
+res = conn.getresponse() 
+data = res.read() 
+
+print(data.decode("utf-8"))
