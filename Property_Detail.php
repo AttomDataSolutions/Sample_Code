83c582f1ccf9de96e2e4a2509e980ba0b94fbc7d
Kevin Mattice <kmattice@onboardinformatics.com>
2017-09-13 15:46:07 -0400

    Create Property_Detail.php
    Sample code for making a property detail call using PHP

---
+++ Property_Detail.php
@@ -0,0 +1,23 @@
+<?php 
+
+$request = new HttpRequest(); 
+$request->setUrl('https://search.onboard-apis.com/propertyapi/v1.0.0/property/detail&#39;); 
+$request->setMethod(HTTP_METH_GET); 
+
+$request->setQueryData(array( 
+  'address1' => '4529 Winona Court', 
+  'address2' => 'Denver, CO' 
+)); 
+
+$request->setHeaders(array( 
+  'apikey' => '870e26a0ffcc29cbb6b8bc86012a8c28', 
+  'accept' => 'application/json' 
+)); 
+
+try { 
+  $response = $request->send(); 
+
+  echo $response->getBody(); 
+} catch (HttpException $ex) { 
+  echo $ex; 
+}
