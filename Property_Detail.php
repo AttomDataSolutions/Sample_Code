1b804ad41201581ff96134b207dcaa9b7d512c32
Kevin Mattice <kmattice@onboardinformatics.com>
2017-11-06 15:34:40 -0500

    Update Property_Detail.php

--- Property_Detail.php
+++ Property_Detail.php
@@ -1,23 +1,28 @@
 <?php 
 
-$request = new HttpRequest(); 
-$request->setUrl('https://search.onboard-apis.com/propertyapi/v1.0.0/property/detail'); 
-$request->setMethod(HTTP_METH_GET); 
+$curl = curl_init(); 
 
-$request->setQueryData(array( 
-  'address1' => '4529 Winona Court', 
-  'address2' => 'Denver, CO' 
+curl_setopt_array($curl, array( 
+  CURLOPT_URL => "https://search.onboard-apis.com/propertyapi/v1.0.0/property/detail?address1=4529%20Winona%20Court&address2=Denver%2C%20CO",
+  CURLOPT_RETURNTRANSFER => true, 
+  CURLOPT_ENCODING => "", 
+  CURLOPT_MAXREDIRS => 10, 
+  CURLOPT_TIMEOUT => 30, 
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, 
+  CURLOPT_CUSTOMREQUEST => "GET", 
+  CURLOPT_HTTPHEADER => array( 
+    "accept: application/json", 
+    "apikey: ", 
+  ), 
 )); 
 
-$request->setHeaders(array( 
-  'apikey' => '870e26a0ffcc29cbb6b8bc86012a8c28', 
-  'accept' => 'application/json' 
-)); 
+$response = curl_exec($curl); 
+$err = curl_error($curl); 
 
-try { 
-  $response = $request->send(); 
+curl_close($curl); 
 
-  echo $response->getBody(); 
-} catch (HttpException $ex) { 
-  echo $ex; 
+if ($err) { 
+  echo "cURL Error #:" . $err; 
+} else { 
+  echo $response; 
 }
