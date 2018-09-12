1ccf1fb1016240457ad5454afddc937d01c1ff53
CodeStoneK <rangaraya@yahoo.com>
2018-09-12 13:31:53 -0700

    Re-did the C# sample to use plain .net assemblies (will create a separate sample with RestSharp)

--- Property_Detail_Call.cs
+++ Property_Detail_Call.cs
@@ -1,5 +1,46 @@
-var client = new RestClient("https://search.onboard-apis.com/propertyapi/v1.0.0/property/detail?address1=4529%20Winona%20Court&address2=Denver%2C%20CO");
-var request = new RestRequest(Method.GET); 
-request.AddHeader("apikey", "870e26a0ffcc29cbb6b8bc86012a8c28"); 
-request.AddHeader("accept", "application/json"); 
-IRestResponse response = client.Execute(request);
+using System;
+using System.Net.Http;
+using System.Xml;
+
+namespace CSHttpClientSample
+{
+    static class Program
+    {
+        static void Main()
+        {
+            MakeApiRequest();
+
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        static async void MakeApiRequest()
+        {
+            var client = new HttpClient();
+
+            /////////////////////////////////////////////////////////////
+            ///
+            ///  MODIFY THESE INPUT VALUES TO SUITE YOUR REQUIREMENTS
+            /// 
+            /////////////////////////////////////////////////////////////
+            var apiKey = "###### your api key here ######";
+            var headerAccept = "application/xml";
+            var inputAddress = "1731 INGRAM TER, DELTONA, FL 32725";
+            /////////////////////////////////////////////////////////////
+
+            client.DefaultRequestHeaders.Add("apikey", apiKey);
+            client.DefaultRequestHeaders.Add("accept", headerAccept);
+
+            var uri = string.Format("https://search.onboard-apis.com/propertyapi/v1.0.0/property/detail?address={0}", inputAddress);
+
+            string response = await client.GetStringAsync(uri);
+
+            // DISPLAY RESPONSE TO CONSOLE
+            Console.WriteLine(response);
+
+            // DESERIALIZE TO XML DOC FOR QUERYING
+            var xml = new XmlDocument();
+            xml.LoadXml(response);
+        }
+    }
+}
