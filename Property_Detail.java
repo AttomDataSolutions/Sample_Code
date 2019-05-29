d7e1841122d3e07346cac6c0cda73d51b4ad69eb
Ranga K <rangaraya@yahoo.com>
2019-05-29 11:13:15 -0700

    Updated the API base URL

--- Property_Detail.java
+++ Property_Detail.java
@@ -1,7 +1,7 @@
 OkHttpClient client = new OkHttpClient(); 
 
 Request request = new Request.Builder() 
-  .url("https://search.onboard-apis.com/propertyapi/v1.0.0/property/detail?address1=4529%20Winona%20Court&address2=Denver%2C%20CO")
+  .url("https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/detail?address1=4529%20Winona%20Court&address2=Denver%2C%20CO")
   .get() 
   .addHeader("accept", "application/json") 
   .addHeader("apikey", "870e26a0ffcc29cbb6b8bc86012a8c28") 
